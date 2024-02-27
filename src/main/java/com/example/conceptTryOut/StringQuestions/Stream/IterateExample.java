9f16f8b7dc8006bfc6903783f20516a6c9525351
kicktocode <ankita.hydbd@gmail.com>
2024-02-27 20:01:00 +0530

    Tried out filter and reduce operation on Product object

--- src/main/java/com/example/conceptTryOut/StringQuestions/Stream/IterateExample.java
+++ src/main/java/com/example/conceptTryOut/StringQuestions/Stream/IterateExample.java
@@ -1,10 +1,12 @@
 package com.example.conceptTryOut.StringQuestions.Stream;
 
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class IterateExample {
     public static void main(String[] args) {
-        Stream.iterate(1,e->e+1).filter(e->e%2==0).limit(10).forEach(System.out::println);
+        Stream.iterate(1, e -> e + 1) // start value is 1, with step increase as second functional operator
+                .filter(e -> e % 2 == 0)   // only even number will proceed to next step.
+                .limit(10)  // only first 10 even numbers will proceed and the stream will be closed.
+                .forEach(System.out::println); // simply prints the stream elements.
     }
 }
--- src/main/java/com/example/conceptTryOut/StringQuestions/Stream/StreamExample.java
+++ src/main/java/com/example/conceptTryOut/StringQuestions/Stream/StreamExample.java
@@ -2,27 +2,37 @@ package com.example.conceptTryOut.StringQuestions.Stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 public class StreamExample {
     public static void main(String[] args) {
 
-
         List<Product> productsList = new ArrayList<Product>();
+//        long  l = 11111L;
+
         //Adding Products
-        productsList.add(new Product(1, "HP Laptop", 25000f));
-        productsList.add(new Product(2, "Dell Laptop", 30000f));
+        productsList.add(new Product(1, "HP Laptop", 25000F));
+        productsList.add(new Product(2, "Dell Laptop", 30000F));
         productsList.add(new Product(3, "Lenevo Laptop", 28000f));
         productsList.add(new Product(4, "Sony Laptop", 28000f));
         productsList.add(new Product(5, "Apple Laptop", 90000f));
 
-        List<Float>minPriceProduct=productsList.stream().filter(p->p.price==28000)
-                .map(p->p.price).collect(Collectors.toList());
+        List<Float> minPriceProduct =
+                productsList.stream()
+                        .filter(p -> p.price == 28000)
+                        .map(p -> p.price)
+                        .collect(Collectors.toList());
         System.out.println(minPriceProduct);
 
-        productsList.stream().filter(p->p.price>28000).forEach(p-> System.out.println(p.name));
-        productsList.stream().filter(f->f.price>30000).forEach(f->System.out.println(f.name));
+        productsList.stream().filter(p -> p.price > 28000).forEach(p -> System.out.println(p.name));
+        productsList.stream().filter(f -> f.price > 30000).forEach(f -> System.out.println(f.name));
 
-        productsList.stream().filter(j->j.price<28000).forEach(j->System.out.println(j.name));
+        productsList.stream().filter(j -> j.price < 28000).forEach(j -> System.out.println(j.name));
+
+        float sumReduceMethod = productsList.stream()
+                .map(product -> product.price)
+                .reduce(0.0f, (x, y) -> x + y);
+        System.out.println(sumReduceMethod);
     }
 }
